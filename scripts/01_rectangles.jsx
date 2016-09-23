def3443bdc413fa71940575e74601af39597676a
trych <trych@users.noreply.github.com>
2016-09-23 15:48:19 +0200

    Add files via upload

---
+++ scripts/01_rectangles.jsx
@@ -0,0 +1,1 @@
+﻿//@includepath "~/Documents/;%USERPROFILE%Documents";//@include "basiljs/basil.js";function draw() {  b.clear(b.doc());  b.units(b.MM);  b.noFill();  var side = 50;    b.rect(0, 0, side, side);  b.rect(0, 0, side * 2, side * 2);  b.rect(0, 0, side * 3, side * 3);}b.go();
