d77e418c5dd14cdf0743276e1b58fb13cb65cea9
Leduen <leduen.oezdemir@gmx.de>
2016-10-09 23:42:07 +0200

    Update 02_Kreativaufgabe_Ledün_01.jsx

--- Uebungen/Loesungen/02_Kreativaufgabe_Ledün_01.jsx
+++ Uebungen/Loesungen/02_Kreativaufgabe_Ledün_01.jsx
@@ -1,1 +1,31 @@
-﻿//@includepath "~/Documents/;%USERPROFILE%Documents";//@include "basiljs/basil.js";function draw() {b.clear(b.doc());b.units(b.MM);for(var i = 0; i < 42; i++){  b.stroke(b.color(b.random(255),0, 0));   b.noFill();  var posX = b.random(0, 400);  var posY = b.random(0, 400);  var randomWidth = b.random(10, 100);  var randomHeigt = b.random(10, 100);  b.rect(posX, posY, randomWidth, randomHeigt);  var rotation =b. radians (12);  b.println (rotation);  b.rotate (rotation);}  }b.go();
+﻿//@includepath "~/Documents/;%USERPROFILE%Documents";
+//@include "basiljs/basil.js";
+
+function draw() {
+
+b.clear(b.doc());
+
+b.units(b.MM);
+
+
+for(var i = 0; i < 42; i++){
+  b.stroke(b.color(b.random(255),0, 0)); 
+  b.noFill();
+  var posX = b.random(0, 200);
+  var posY = b.random(0, 200);
+
+  var randomWidth = b.random(10, 100);
+  var randomHeigt = b.random(10, 100);
+  b.rect(posX, posY, randomWidth, randomHeigt);
+  var rotation =b. radians (12);
+  b.println (rotation);
+  b.rotate (rotation);
+
+}
+
+
+
+  
+}
+
+b.go();
