2167d0d2f101971d860e698d2892a3165500f112
trych <timorychert@23x5.net>
2016-10-14 15:26:18 +0200

    Adding scripts of units 3 and 4

---
+++ scripts/03_textLength.jsx
@@ -0,0 +1,1 @@
+﻿//@includepath "~/Documents/;%USERPROFILE%Documents";//@include "basiljs/basil.js";function draw() {  var input = prompt("Bitte einen Text eingeben.", "Beispiel");  var inputLength = input.length;  alert("Der Text " + input + " ist " + inputLength + " Buchstaben lang.");}b.go();
---
+++ scripts/03_weekday.jsx
@@ -0,0 +1,1 @@
+﻿//@includepath "~/Documents/;%USERPROFILE%Documents";//@include "basiljs/basil.js";function draw() {  b.weekday()  if( b.weekday() == "Wednesday" ) {    b.text("Heute ist Mittwoch!", 0, 0, 100, 100);  } else {    b.text("Heute ist nicht Mittwoch.", 0, 0, 100, 100)  }}b.go();
