d60090356ed53096dd0162aa44d1ee5c0b54af46
trych <trych@users.noreply.github.com>
2016-10-18 15:50:16 +0200

    Rename 02_Kreativaufgabe_Ledün_02.jsx to 02_Kreativaufgabe_Leduen_02.jsx

---
+++ Uebungen/Loesungen/02_Kreativaufgabe_Leduen_02.jsx
@@ -0,0 +1,31 @@
+﻿//@includepath "~/Documents/;%USERPROFILE%Documents";
+//@include "basiljs/basil.js";
+
+function draw() {
+
+b.clear(b.doc());
+
+b.units(b.MM);
+
+
+for(var i = 0; i < 42; i++){
+  b.stroke(b.color(b.random(255),0, 0)); 
+  b.noFill();
+
+  var posX = 100;
+  var posY = 150;
+  var randomWidth = b.random(10, 100);
+  var randomHeigt = b.random(10, 100);
+  b.rect(posX, posY, randomWidth, randomHeigt);
+  var rotation =b. radians (12);
+  b.println (rotation);
+  b.rotate (rotation);
+
+}
+
+
+
+  
+}
+
+b.go();
