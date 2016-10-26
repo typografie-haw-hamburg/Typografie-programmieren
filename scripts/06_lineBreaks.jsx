be18b6bc19266284ea90aba1e0596a5a6e8c7432
trych <timorychert@23x5.net>
2016-10-27 00:43:34 +0200

    Adding course scripts 06

---
+++ scripts/06_lineBreaks.jsx
@@ -0,0 +1,1 @@
+﻿//@includepath "~/Documents/;%USERPROFILE%Documents";//@include "basiljs/basil.js";function draw() {  b.clear(  b.doc()  );  b.text("Umbruch\n mit \\n:\nNeue Zeile", 100, 100, 100, 100);  b.text("Umbruch\r mit \\r:\rNeuer Absatz", 200, 200, 100, 100);}b.go();
