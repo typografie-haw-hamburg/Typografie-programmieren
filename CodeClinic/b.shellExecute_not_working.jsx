66abff9076014ba86735ce48934a70ee59cd81a8
Baesil <dvaheb@hotmail.com>
2016-10-26 16:09:44 +0200

    Update b.shellExecute_not_working.jsx

--- CodeClinic/b.shellExecute_not_working.jsx
+++ CodeClinic/b.shellExecute_not_working.jsx
@@ -1,1 +1,17 @@
-﻿//@includepath "~/Documents/;%USERPROFILE%Documents";//@include "basiljs/basil.js";function draw() {//Der wget-command (MyCode) soll die Jpegs der URL in den von mir erstellten Ordner herunterladen.var MyCode = "wget  -r -l 3 -np -nd -p -P /Users/Darius/Documents/basiljs/Download  -A jpg,jpeg  http://www.booooooom.com/2016/10/25/photographer-spotlight-jesse-salto/ -S";b.shellExecute( MyCode );}b.go();
+﻿//@includepath "~/Documents/;%USERPROFILE%Documents";
+//@include "basiljs/basil.js";
+
+function draw() {
+
+//Der wget-command (MyCode) soll die Jpegs der URL in den von mir erstellten Ordner herunterladen.
+//Darius
+
+var MyCode = "wget  -r -l 3 -np -nd -p -P /Users/Darius/Documents/basiljs/Download  -A jpg,jpeg  http://www.booooooom.com/2016/10/25/photographer-spotlight-jesse-salto/ -S";
+
+b.shellExecute( MyCode );
+
+
+
+}
+
+b.go();
