b0c849c4288738dadc5d5a03f841e260ea9111f3
Doeroe <dorothee.schraudner@gmail.com>
2016-12-19 17:10:19 +0100

    Add files via upload

---
+++ CodeClinic/jsonTest.jsx
@@ -0,0 +1,1 @@
+﻿//@includepath "~/Documents/;%USERPROFILE%Documents";//@include "basiljs/basil.js";function draw() {  b.clear(b.doc());  b.println(1);  //JSON  var jsonString = b.loadString("161115_sound_short.json");  b.println(2);  var jsonData = b.JSON.decode( jsonString );   b.println(3);}b.go();
