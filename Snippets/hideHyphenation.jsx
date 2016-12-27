c765a57fbd36746b1175083429930d60096f2a75
trych <timorychert@23x5.net>
2016-12-27 22:59:29 +0100

    Added .eslintrc and hideHyphenation.jsx snippet

---
+++ Snippets/hideHyphenation.jsx
@@ -0,0 +1,35 @@
+﻿// @includepath "~/Documents/;%USERPROFILE%Documents";
+// @include "basiljs/basil.js";
+
+
+// how to find hyphenated words in a text
+
+function draw() {
+
+  b.clear( b.doc() );
+
+  // create a text frame and fill it with placeholder text
+  var tf = b.text("", 0, 0, 300, 300);
+  b.placeholder(tf);
+
+  var words = b.words(tf);
+  var hyphCounter = 0;
+
+  // check for each word if there is more than one (parent) line, if so, it's hyphenated
+  for (var i = 0; i < words.length; i++) {
+    if(words[i].lines.length > 1) {
+      hyphCounter++;
+      b.println("The word " + words[i].contents + " is hyphenated.");
+
+      // search the last insertion point of each hyphenated line and insert a discretionary line break
+      words[i].lines[0].insertionPoints[-1].contents = SpecialCharacters.DISCRETIONARY_LINE_BREAK;
+    }
+  }
+
+  if(hyphCounter === 0) {
+    alert("There were no hyphenated words.");
+  }
+
+}
+
+b.go();
