76fc83b2f5a33723413fd69721144c7a259d9cdc
trych <timorychert@23x5.net>
2017-01-06 18:29:54 +0100

    Linting

--- Projekte/Lucas/sketch.js
+++ Projekte/Lucas/sketch.js
@@ -8,28 +8,28 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   getTweet();
 
-  
-  //console.log(tweetArray);
+
+  // console.log(tweetArray);
 }
 
 function draw() {
   frame[0] = new Frame(300, 100);
   frame[1] = new Frame(300, 200);
   frame[2] = new Frame(300, 300);
-  
+
   frame[0].display();
   frame[1].display();
   frame[2].display();
 }
 
 
-function wordTimer (){
+function wordTimer () {
   index = index + 1;
-    
+
   if (index >= tweetArray.length) {
     index = 0;
   }
-  //console.log(index);
+  // console.log(index);
 }
 
 
@@ -38,31 +38,30 @@ function Frame (x, y) {
   this.y = y;
   this.sizeX = 400;
   this.sizeY = 100;
- 
+
   this.display = function () {
-    //noStroke();
+    // noStroke();
     fill(255);
     rectMode(CENTER);
     rect(this.x, this.y, this.sizeX, this.sizeY);
-    
+
     fill(0);
     textSize(72);
     textAlign(CENTER, CENTER);
     text(tweetArray[index], this.x, this.y);
-    //tweetArray.splice(index, 1);
+    // tweetArray.splice(index, 1);
     console.log(tweetArray.length);
-  }
+  };
 }
 
 function getTweet () {
-  //var tweetRecent = "" + tweetIncoming + "";
+  // var tweetRecent = "" + tweetIncoming + "";
   var tweetRecent = "Hallo Timo, mein Script kann schon Wörter aufteilen und nach und nach anzeigen";
   var splitTweet = split(tweetRecent, " ");
   for (var i = 0; i < splitTweet.length; i++) {
     tweetArray.push(splitTweet[i]);
   }
-    //console.log(splitTweet);
+    // console.log(splitTweet);
 }
-
 
 
