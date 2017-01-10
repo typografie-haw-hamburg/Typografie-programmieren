e52905808887a133069e370c1b25f07bfe0ea81c
LucasKra <lucas_kramer@hotmail.de>
2017-01-10 18:43:31 +0100

    Update server.js

--- Projekte/Lucas/2017_01_10/server.js
+++ Projekte/Lucas/2017_01_10/server.js
@@ -1,3 +1,5 @@
+// hey timo, die wörter habe ich im server.js aufgesplittet und in das objekt als array gespeichert, das läuft soweit (siehe terminal). die tweets werden noch zu schnell geladen und der wordIndex, der die Worte sekündlich hochzählen soll, kommt nicht hinterher. pro tweet wird nun ein wort angezeigt, während der timer hochzählt. die idee war ja sowieso, den text im frame zu laden und die getTweet-funktion nur dazu benutzen, einen tweet zu laden und in den array als objekt zu pushen. der frame soll dann an der stelle tweetIndex durch die wörter an der stelle wordIndex loopen. wenn der wordIndex > word.length, wordIndex = 0, und im draw loop if wordIndex = 0, getTweet. das aktuelle problem ist nun, dass er die länge des arrays irgendwie nicht erkennt, also der wordIndex nie wieder zurückgesetzt wird.
+
 // Reads a twitter stream and sends tweets from Node.js to p5.js
 
 
