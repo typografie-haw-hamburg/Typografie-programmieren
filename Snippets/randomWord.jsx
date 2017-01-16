9704921bd501a4a31214bf5c170edf43d848932d
trych <timorychert@23x5.net>
2017-01-16 18:06:23 +0100

    Added randomWord.jsx script

---
+++ Snippets/randomWord.jsx
@@ -0,0 +1,31 @@
+﻿// @includepath "~/Documents/;%USERPROFILE%Documents";
+// @include "basiljs/basil.js";
+
+
+// how to find a random word in a flowing text
+
+
+function draw() {
+
+  b.clear( b.doc() );
+
+
+  // create two text frames, connect them and fill them with placeholder text
+  var tf1 = b.text("", 0, 0, 200, 400);
+  var tf2 = b.text("", 300, 100, 200, 400);
+
+  b.linkTextFrames(tf1, tf2);
+  b.placeholder(tf1);
+
+
+  // save the entire text of the textframes (the "story") in a variable, split it into words and pick a random word
+  var entireText = tf1.parentStory;
+  var words = b.words(entireText);
+
+  var randomIndex = b.floor( b.random(0, words.length));
+  var randomWord = words[randomIndex];
+
+  b.println("The random word is word no. " + (randomIndex + 1) + ": " + randomWord.contents);
+}
+
+b.go();
