fcd27b89852b2739b0a485687ad008c66218dfb8
trych <timorychert@23x5.net>
2017-01-27 17:41:53 +0100

    Added moveLayer.jsx snippet

---
+++ Snippets/moveLayer.jsx
@@ -0,0 +1,31 @@
+﻿// @includepath "~/Documents/;%USERPROFILE%Documents";
+// @include "basiljs/basil.js";
+
+
+// how to apply a characterStyle to some text
+
+
+function draw() {
+
+  // close open doc without saving
+  b.close(false);
+
+  var tf = b.text(b.LOREM, 0, 0, 200, 200);
+
+  // get layer of text frame and rename it
+  var textLayer = tf.itemLayer;
+  textLayer.name = "Text";
+
+  // create two new layers
+  b.layer("Images");
+  b.layer("Shapes");
+
+
+  // move text layer to the front
+  textLayer.move(LocationOptions.AT_BEGINNING);
+
+  // for other moving options see: http://www.indesignjs.de/extendscriptAPI/indesign8/#Layer.html#d1e175084__d1e175559
+
+}
+
+b.go();
