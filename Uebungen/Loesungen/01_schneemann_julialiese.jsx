eecbc60661668af005964ab593d2092b7f013ed4
lujia23 <32480609+lujia23@users.noreply.github.com>
2017-10-06 11:19:41 +0200

    Add files via upload

---
+++ Uebungen/Loesungen/01_schneemann_julialiese.jsx
@@ -0,0 +1,35 @@
+﻿// @includepath "~/Documents/;%USERPROFILE%Documents";
+// @include "basiljs/basil.js";
+
+function draw() {
+
+b.clear(b.doc());
+b.units(b.MM);
+b.noFill();
+
+b.ellipse(100, 100, 50 ,50);
+b.ellipse(100, 165, 80, 80);
+b.ellipse(100, 250, 90,90);
+
+b.fill(0, 0, 0);
+b.ellipse(90, 90, 5 ,5);
+b.ellipse(110, 90, 5, 5);
+
+b.rect(80,40,40,40);
+
+b.ellipse(100,165,5,5);
+b.ellipse(100,155,5,5);
+b.ellipse(100,145,5,5);
+
+b.ellipse(100,115,5,5);
+
+b.fill(255, 140, 0);
+b.beginShape(b.CLOSE);
+  b.vertex (100, 95);
+  b.vertex(100, 105);
+  b.vertex(80, 100);
+b.endShape();
+
+}
+
+b.go();
