415bc47f91622fa6f43d57181dcb2ed352cb2824
TheoBergmann420 <32646883+TheoBergmann420@users.noreply.github.com>
2017-10-09 18:15:41 +0200

    Add files via upload

---
+++ Uebungen/Loesungen/01_Schneemann_TheoBergmann.jsx
@@ -0,0 +1,50 @@
+﻿// @includepath "~/Documents/;%USERPROFILE%Documents";
+// @include "basiljs/basil.js";
+
+function draw() {
+b.clear( b.doc());
+
+b.units(b.MM);
+b.size(500,600)
+
+b.noFill();
+b.ellipse(125,300,60,60);
+b.ellipse(125,248,45,45);
+b.ellipse(125,208,35,35);
+
+
+
+b.rect(300,200,100,100);
+b.line(300,200,400,300);
+b.line(400,200,300,300);
+b.line(300,200,350,150);
+b.line(400,200,350,150);
+
+b.fill(100,100,100);
+
+b.rect(105,185,40,5);
+b.rect(115,150,20,40);
+
+
+
+b.ellipse(120,205,5,5);
+b.ellipse(130,205,5,5);
+b.ellipse(125,300,5,5);
+b.ellipse(125,300,5,5);
+b.ellipse(125,248,5,5);
+
+
+b.beginShape(b.CLOSE);
+  b.vertex(125.5, 220);
+  b.vertex(123, 213);
+  b.vertex(128, 213);
+b.endShape()
+
+
+
+
+
+
+}
+
+b.go();
