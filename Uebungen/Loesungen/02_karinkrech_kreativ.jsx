43ce7839ec9041e26079bdc1766823f6efc5a01f
karinkolumna <32513868+karinkolumna@users.noreply.github.com>
2017-10-11 15:54:11 +0200

    Add files via upload

---
+++ Uebungen/Loesungen/02_karinkrech_kreativ.jsx
@@ -0,0 +1,27 @@
+﻿// @includepath "~/Documents/;%USERPROFILE%Documents";
+// @include "basiljs/basil.js";
+
+function draw() {
+  b.clear(b.doc());
+  b.units(b.MM);
+  b.noStroke();
+
+  for(var i=0;i<20;i++){
+    var size=b.random(10,100)
+    var x = b.random(size/2,b.width - size/2);
+    var y = b.random(size/2,b.height - size/2);
+    var red = b.random(0,255);
+    var green = b.random(0,255);
+    var blue = b.random(0,255);
+    var randomColors = [
+      b.color(red, green, blue),
+      b.color(green, blue, red)
+    ];
+
+    b.fill(b.gradient(randomColors, "RandomGradient" + i));
+    b.ellipse(x,y,size,size);
+    //20 kreise
+  }
+}
+
+b.go();
