9e08805b976b245ccb24b8c8f715053ad2828f2b
lujia23 <32480609+lujia23@users.noreply.github.com>
2017-10-17 19:30:18 +0200

    Add files via upload

---
+++ Uebungen/Loesungen/03_Kreativaufgabe_JuliaLiese.jsx
@@ -0,0 +1,31 @@
+﻿// @includepath "~/Documents/;%USERPROFILE%Documents";
+// @include "basiljs/basil.js";
+
+function draw() {
+
+b.revert();
+b.units(b.MM);
+
+b.rect(0, 0, b.width, b.height);
+b.rectMode(b.CENTER);
+b.noFill();
+
+for(var i = 0; i < 30; i++){
+var rotate = b.random(23,100);
+
+b.rotate(rotate);
+b.rect(b.width / 2 , i + 130, i * 10, i * 10);
+b.stroke(255 - (i * 10), 59, 247);
+
+if(i < 10){
+b.strokeWeight(5);
+
+}else{
+b.strokeWeight(i+3);
+
+}
+
+}
+}
+
+b.go();
