0b324a0230548f64c63bd338aea8a66c9e00c02e
annadomeyer <32332006+annadomeyer@users.noreply.github.com>
2017-10-31 23:59:45 +0100

    Add files via upload

---
+++ Uebungen/Loesungen/05_Kreativaufgabe_AnnaDomeyer.jsx
@@ -0,0 +1,40 @@
+﻿// @includepath "~/Documents/;%USERPROFILE%Documents";
+// @include "basiljs/basil.js";
+
+function draw() {
+
+  b.clear(b.doc());
+  b.units( b.MM );
+  b.noStroke();
+
+
+
+
+ var tib= b.random(50,100);
+
+      for(var j = 0; j < 11; j++) {
+
+          var menge= b.random(50,150);
+
+
+  var zahl=b.random(5,10);
+  var x= 20;
+  var y= 20;
+  var red = b.color(255, 0, 0);
+  var blue = b.color(0, 0, 255);
+
+
+for(var i = 0; i < 15; i++) {
+      var re= b.random(20,255);
+        var ge= b.random(20,255);
+        var bl= b.random(20,255);
+        b.fill(re,ge,bl);
+
+   b.rect(x*j-5,y*i,20,20);
+
+          }
+}
+}
+
+
+b.go();
