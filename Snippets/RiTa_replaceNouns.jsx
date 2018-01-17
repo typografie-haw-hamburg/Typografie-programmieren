b6e9299caf0d5d8035e129eb34a90269dd36fb59
trych <timorychert@23x5.net>
2018-01-17 20:53:10 +0100

    New snippets

---
+++ Snippets/RiTa_replaceNouns.jsx
@@ -0,0 +1,57 @@
+﻿// @includepath "~/Documents/;%USERPROFILE%Documents";
+// @include "basiljs/basil.js";
+// @include "basiljs/trych/lib/polyfills.js"
+// @include "basiljs/trych/lib/rita-full.js";
+
+
+function draw() {
+
+  b.clear(b.doc());
+
+  // zufälliges noun im Text durch ein zufälliges noun im Lexicon ersetzen
+
+  var myString = "It was a dark and stormy night and a train and some unicorns.";
+  var myRiString = RiString(myString);
+  var lexicon = RiLexicon();
+
+  var myPos = myRiString.pos();  // parts of speech
+  b.println(myPos);
+
+  b.text(myPos, 200, 200, 1000, 1000)
+
+  var allNounPositions = []; // leerer Array in dem alle nouns-Positionen gesammelt werden
+
+  // collect nouns
+  for (var i = 0; i < myPos.length; i++) {
+    if(b.startsWith(myPos[i], "nn")) {
+      allNounPositions.push(i);
+    }
+  }
+
+  b.println("Noun positions: " + allNounPositions);
+
+  var randomIndex = b.floor(b.random(0, allNounPositions.length));
+  var nounPosition = allNounPositions[randomIndex];
+
+  b.println("NounPosition: " + nounPosition);
+
+  var tf = b.text(myString, 0, 0, 100, 100);
+  var myWords = b.words(tf);
+
+  b.typo(myWords[nounPosition], "pointSize", 16);
+  b.typo(myWords[nounPosition], "contents", lexicon.randomWord('nn'));
+
+
+  // Token als Text ausgeben
+
+  var tokenMeanings = {
+    nn: "Nomen",
+    prp: "Präposition"
+  }
+
+  var myToken = "prp";
+
+  b.println(tokenMeanings[myToken]);
+}
+
+b.go();
---
+++ Snippets/spreadTextToTextFrames.jsx
@@ -0,0 +1,29 @@
+﻿// @includepath "~/Documents/;%USERPROFILE%Documents";
+// @include "basiljs/basil.js";
+
+function draw() {
+
+  b.clear(b.doc());
+  b.units(b.MM);
+
+  b.textSize(36);
+  var tf1 = b.text("This is my spectacular headline", 0, 0, 100, 20);
+
+  // wichtig! Wenn der erste Textrahmen gestyled werden soll,
+  // dann sollte das spätestens hier passieren, damit auch final
+  // festgelegt ist, wo der Text umbricht, damit sich das später
+  // nicht noch mal ändert
+
+  if(tf1.overflows === true) {
+    var tf2 = b.text("", 0, 40, 100, 20);
+    b.linkTextFrames(tf1, tf2);
+  }
+
+  // hier kann dann der zweite Textrahmen anders gestyled werden
+  // b.applyParagraphStyle(tf2, "meinAbsatzformat");
+  // b.typo(tf2, "skew", 48);
+  // etc.
+
+}
+
+b.go();
